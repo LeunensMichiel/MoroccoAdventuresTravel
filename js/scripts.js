3ec7c4994bcf9e8f3821b15b4e3a13222efaf0d6
Michiel Leunens <michiel.leunens@gmail.com>
2019-04-13 12:58:05 +0200

    Added Carousel and title for main page

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // // Mobile True Viewport Height
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+    // document.querySelector('#togglemenu').checked = false;
+
+    // // Open Navbar
+    // document.querySelector('#togglemenu').addEventListener('click', () => {
+    //     document.querySelector('#menu-overlay').classList.toggle('open');
+    //     document.querySelector('.wrapper').classList.toggle('closed');
+    // });
+
+    // document.querySelector('.link').addEventListener('click', () => {
+    //     document.querySelector('#togglemenu').checked = false;
+    // });
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'fade',
+        speed: 800,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        grabCursor: true,
+        loop: true,
+        autoplay: {
+            delay: 6500,
+          },
+    });
+
+});
